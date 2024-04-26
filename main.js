476f801a2704037425ae2a9282c06d3e2aa786fc
jonatan gomes floriano <160983484+jonatancolt@users.noreply.github.com>
2024-04-26 16:18:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,66 +1,21 @@
-// Capturar evento de submit do formulário
-const form = document.querySelector('#formulario');
+const btn = document.querySelector(btn);
+btn.addEventListener('click', () => {
+    btn.style.backgroundColor = '#ffac09';
+})
+function esconde(btn) {
+    document.getElementById(btn).style.display = 'none';
 
-form.addEventListener('submit', function (e) {
-  e.preventDefault();
-  const inputPeso = e.target.querySelector('#peso');
-  const inputAltura = e.target.querySelector('#altura');
-
-  const peso = Number(inputPeso.value);
-  const altura = Number(inputAltura.value);
-
-  if (!peso) {
-    setResultado('Peso inválido', false);
-    return;
-  }
-
-  if (!altura) {
-    setResultado('Altura inválida', false);
-    return;
-  }
-
-  const imc = getImc(peso, altura);
-  const nivelImc = getNivelImc(imc);
-
-  const msg = `Seu IMC é ${imc} (${nivelImc}).`;
-
-  setResultado(msg, true);
-});
-
-function getNivelImc (imc) {
-  const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso',
-    'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
-
-  if (imc >= 39.9) return nivel[5];
-  if (imc >= 34.9) return nivel[4];
-  if (imc >= 29.9) return nivel[3];
-  if (imc >= 24.9) return nivel[2];
-  if (imc >= 18.5) return nivel[1];
-  if (imc < 18.5) return nivel[0];
 }
 
-function getImc (peso, altura) {
-  const imc = peso / altura ** 2;
-  return imc.toFixed(2);
+function mostra(btn){
+    document.getElementById(btn).style.display = 'block';
 }
+function toggle(btn) {
+    var display = document.getElementById(btn).style.display;
+    if(display == "none"){
+        document.getElementById(btn).style.display = 'block';
 
-function criaP () {
-  const p = document.createElement('p');
-  return p;
-}
-
-function setResultado (msg, isValid) {
-  const resultado = document.querySelector('#resultado');
-  resultado.innerHTML = '';
-
-  const p = criaP();
-
-  if (isValid) {
-    p.classList.add('paragrafo-resultado');
-  } else {
-    p.classList.add('bad');
-  }
-
-  p.innerHTML = msg;
-  resultado.appendChild(p);
-}
+    }
+    else{
+        document.getElementById('.btn').style.display = 'none';
+}}
